3d446cd1231562d005095efc80003229658ce499
paidajunjace <paidajunjace@users.noreply.github.com>
2026-09-22 23:49:43 +0000

    fix: Douyu cookie must inject as Dart raw string; add missing DouyuAccountService import

--- simple_live_app/lib/main.dart
+++ simple_live_app/lib/main.dart
@@ -25,6 +25,7 @@ import 'package:simple_live_app/routes/app_pages.dart';
 import 'package:simple_live_app/routes/route_path.dart';
 import 'package:simple_live_app/services/bilibili_account_service.dart';
 import 'package:simple_live_app/services/douyin_account_service.dart';
+import 'package:simple_live_app/services/douyu_account_service.dart';
 import 'package:simple_live_app/services/db_service.dart';
 import 'package:simple_live_app/services/follow_service.dart';
 import 'package:simple_live_app/services/local_storage_service.dart';
